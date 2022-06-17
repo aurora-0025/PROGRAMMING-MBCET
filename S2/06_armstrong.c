c32f92f639b14e4b9219989f21d942541031ec0e
Rhon <98303094+Rhogneld@users.noreply.github.com>
2022-06-17 18:40:31 +0530

    Update 06_armstrong.c

--- S2/06_armstrong.c
+++ S2/06_armstrong.c
@@ -1,17 +1,26 @@
 // Read a Natural Number and check whether the number is Armstrong or not
 #include <stdio.h>
+#include <math.h>
 void main()
 {
-    int num, temp;
+    int num, temp,temp1;
     int sum = 0;
+    int d=0;
     printf("Enter a number to check if its armstrong or not: ");
     scanf("%d", &num);
-    temp = num;
-    while (temp > 0)
+    temp=num;
+    temp1=num;
+    
+    while(temp>0)
     {
-        int digit = temp % 10;
-        sum = sum + digit * digit * digit;
-        temp = temp / 10;
+       d++;
+       temp=temp/10;
+    }
+    
+    while (temp1 > 0)
+    {
+        sum = sum + pow(temp1%10,d);
+        temp1 = temp1 / 10;
     }
     if (sum == num)
         printf("%d is an armstrong number.", num);
