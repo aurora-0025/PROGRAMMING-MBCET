352c401094de024dad6014ba5263759927698a51
Rhon <98303094+Rhogneld@users.noreply.github.com>
2022-07-22 19:21:13 +0530

    Update 14_vowelconsonantspace.c

--- S2/14_vowelconsonantspace.c
+++ S2/14_vowelconsonantspace.c
@@ -1,18 +1,22 @@
 // Read a string (ending with a $ symbol), store it in an array and count the number of vowels, consonants and spaces in it.
 
 #include <stdio.h>
+#include <string.h>
 
 void main()
 {
     int vowelCount, consonantCount, spaceCount;
     char str[50];
-
     vowelCount = consonantCount = spaceCount = 0;
     printf("Enter a string ending with $ :");
-    fgets(str, 50, stdin);
+    scanf("%[^$]s", str);
 
-    for (int i = 0; str[i] != '$'; i++)
+    for (int i = 0; i < strlen(str); i++)
     {
+
+        if (str[i] >= 65 && str[i] <= 90)
+            str[i] = str[i] + 32;
+
         if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u')
         {
             vowelCount = vowelCount + 1;
