3a45a6d7e5134a0db5fdb98802fd0aa4638619e8
Ritin George <78461388+aurora-0025@users.noreply.github.com>
2022-08-09 19:28:40 +0100

    Update 16_sumdigits.c

--- S2/16_sumdigits.c
+++ S2/16_sumdigits.c
@@ -2,17 +2,20 @@
 
 #include <stdio.h>
 
+int sumDigits(int);
+
 void main()
 {
     int num, sum;
     sum = 0;
     printf("Enter a number: ");
     scanf("%d", &num);
-    printf("Sum of all digits of %d = ", num);
-    while(num > 0)
-    {
-        sum = sum + num%10;
-        num = num/10;
-    }
-    printf("%d\n", sum);
+	sum = sumDigits(num);
+    printf("Sum of all digits of %d = %d", num, sum);
+}
+
+int sumDigits(int n)
+{
+	if(n == 0) return 0;
+	return (n%10 + sumDigits(n/10));
 }
