8c7604d1cf99629cbc393678a8bcf0ddcd0ac6db
Ritin George <78461388+aurora-0025@users.noreply.github.com>
2022-08-09 18:57:40 +0100

    Create 21_filewordscharslines.c

---
+++ S2/21_filewordscharslines.c
@@ -0,0 +1,56 @@
+/*
+Open a text input file and count the number of characters, words and lines in it, and store the
+results to an output file.
+*/
+
+#include <stdio.h>
+
+void main()
+{
+	FILE *fp;
+	char ch;
+	int noWords=1, noLines=1, noChars=0;
+
+	fp = fopen("input.txt", "r");
+	if(fp == NULL) 
+	{
+		printf("File Not Found\n");
+	}
+	else {
+		printf("Input file contents..\n");
+		while((ch=fgetc(fp))!=EOF)
+		{
+			printf("%c", ch);
+			if(ch == ' ') noWords++;
+			else if(ch == '\n') noLines++;
+			else noChars++;
+		}
+		printf("\n\n");
+		fclose(fp);
+    
+    	printf("Writing output to output.txt\n");
+    
+    	fp = fopen("output.txt", "w");
+    	fprintf(fp, "No of Words: %d\nNo of Lines: %d\nNo of Characters: %d", noWords, noLines, noChars); 
+    	printf("No of Words: %d\nNo of Lines: %d\nNo of Characters: %d\n", noWords, noLines, noChars); 
+    	fclose(fp);
+    }
+
+}
+
+/*
+Output 
+======
+
+Input file contents..
+This is a random
+Sentence with
+multiple lines
+
+
+Writing output to output.txt
+No of Words: 6
+No of Lines: 4
+No of Characters: 38
+
+*/
