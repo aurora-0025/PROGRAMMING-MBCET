362daf7fe22c7919562cbcb48c5b423a48ecbb33
Ritin George <78461388+aurora-0025@users.noreply.github.com>
2022-08-09 18:52:44 +0100

    Create 20_file_operations.c

---
+++ S2/20_file_operations.c
@@ -0,0 +1,60 @@
+/*
+Program to create a file and do the following operations
+1) write to file
+2) read from file and display it in console
+3) append data to file
+*/
+
+#include <stdio.h>
+void main()
+{
+	FILE *fp;
+	int isRunning = 1, choice;
+	char str[50], ch;
+	do
+	{
+		printf("\n\n1) To write to file\n");
+		printf("2) To read from file\n");
+		printf("3) To append to file\n");
+		printf("4) To exit\n\n");
+		printf("Enter Your Choice: ");
+		scanf("%d", &choice);
+		getchar();
+		switch(choice)
+		{
+			case 1:
+				fp=fopen("file1.txt", "w");
+				printf("Enter data to write to file: ");
+				scanf("%[^\n]s", str);
+				printf("Wrote data to file...\n");	
+				fprintf(fp, "%s", str);
+				fclose(fp);
+				break;
+			case 2:
+				fp=fopen("file1.txt", "r");
+				if(fp == NULL) {
+					printf("There is no file to read please write data to file first");
+					break;
+				}
+				printf("Read data from file...\n");
+				while((ch=fgetc(fp))!=EOF) printf("%c", ch);
+				printf("\n");
+				fclose(fp);
+				break;
+			case 3:
+				fp=fopen("file1.txt", "a");
+				printf("Enter data to append to file: ");
+				scanf("%[^\n]s", str);
+				printf("Appended data to file...\n");
+				fprintf(fp, "%s", str);
+				fclose(fp);
+				break;
+			case 4:
+				printf("Exiting Program");
+				isRunning=0;
+				break;
+			default:
+				printf("Invalid Input");
+		}
+	} while(isRunning);
+}
