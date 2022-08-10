c935dc8440d2e09ec354fb1c2325cedb1474fe0f
Ritin George <78461388+aurora-0025@users.noreply.github.com>
2022-08-10 18:38:55 +0100

    Update 21_filewordscharslines.c

--- S2/21_filewordscharslines.c
+++ S2/21_filewordscharslines.c
@@ -4,40 +4,43 @@ results to an output file.
 */
 
 #include <stdio.h>
+
 void main()
 {
-    FILE *fp;
-    char ch;
-    int wc = 1, chc = 0, lc = 1;
-    int f = 0;
-    fp = fopen("sample.txt", "r");
-    if (fp == NULL)
-    {
-        printf("File not found\nCheck file name\n");
-        f = 1;
-    }
-    else
-    {
+	FILE *fp;
+	char ch;
+	int noWords = 0, noLines = 0, noChars = 0;
 
-        while ((ch = fgetc(fp)) != EOF)
-        {
-            if (ch == ' ' || ch == '.' || ch == '\n')
-                wc++;
-            if (ch == '\n')
-                lc++;
-            else if (ch != ' ')
-                chc++;
-        }
-    }
-    rewind(fp);
-    if (f == 0)
-    {
-        printf("The file is:\n");
-        while ((ch = fgetc(fp)) != EOF)
-            printf("%c", ch);
-        printf("\n");
-        printf("\nTotal number of words = %d\n", wc);
-        printf("Total number of lines = %d\n", lc);
-        printf("Total number of characters = %d\n", chc);
-    }
+	fp = fopen("input.txt", "r");
+	if (fp == NULL)
+	{
+		printf("File Not Found\n");
+	}
+	else
+	{
+		printf("Input file contents..\n");
+		while ((ch = fgetc(fp)) != EOF)
+		{
+			printf("%c", ch);
+			noChars++;
+			if (ch == '\n')
+				noLines++;
+			if (ch == ' ' || ch == '\t' || ch == '\n')
+				noWords++;
+		}
+		if (noChars > 0)
+		{
+			noWords++;
+			noLines++;
+		}
+		printf("\n\n");
+		fclose(fp);
+
+		printf("Writing output to output.txt\n");
+
+		fp = fopen("output.txt", "w");
+		fprintf(fp, "No of Words: %d\nNo of Lines: %d\nNo of Characters: %d", noWords, noLines, noChars);
+		printf("No of Words: %d\nNo of Lines: %d\nNo of Characters: %d\n", noWords, noLines, noChars);
+		fclose(fp);
+	}
 }
